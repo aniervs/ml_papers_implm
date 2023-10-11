ecb1967b79fda4b510cf8759f833281793c81cf8
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-10-11 02:48:24 +0200

    Add utils file with useful stuff used everywhere.

---
+++ utils.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda:0")
+    if torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
+
+
+class BigClassifier(nn.Module):
+    def __init__(self, in_features, no_classes):
+        super().__init__()
+
+        self.sequential = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, no_classes),
+        )
+
+    def forward(self, img):
+        return self.sequential(img)
+
+
+def train(model, train_loader, optimizer, loss_function, epoch, device):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.to(device)
+        target = target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_function(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 200 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, test_loader, loss_function, device):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+            test_loss += loss_function(output, target).sum().item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    return correct / len(test_loader.dataset)
+
