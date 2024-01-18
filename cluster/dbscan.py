9643f687b35bf8bff9a0ddba8206aa76ca714a57
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2024-01-18 06:07:18 +0100

    FIX BUG. Add DBSCAN algorithm implementation with distance metrics and test function

--- cluster/dbscan.py
+++ cluster/dbscan.py
@@ -1,82 +1,114 @@
 # PAPER: https://www.dbs.ifi.lmu.de/Publikationen/Papers/KDD-96.final.frame.pdf
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs, make_moons 
+from sklearn.preprocessing import StandardScaler
 
-cluster_id = [None] # tells for each point, its cluster_id
-points = [None] # array of points
-eps = 1e-3 # change this
-min_pts = 1 # change this
-
-def distance(a, b):
-    # euclidean
-    result = 0
-    k = len(a)
-    for i in range(k):
-        result += (a[i] - b[i])**2
-    return result 
-
-def region_query(p):
+def distance(p1, p2, metric='euclidean'):
     """
-    p: a point
-    eps: threshold on the neighborhood distance
+    p1, p2: points
+    metric: distance metric
     
-    return: list of ids of points at distance < eps from p
-    
-    (done with Brute Force now)
-    # TODO: change later to R*-trees
+    return: distance between p1 and p2
     """
-    result_ids = []
-    n = len(points)
-    
-    for i in range(n):
-        if distance(points[i], p) < eps:
-            result_ids.append(i)
-    return result_ids  
-
-def expand_cluster(p_idx, n_clusters):
-    """
-    p_idx: id of the point candidate to be core of a cluster
-    n_clusters: number of clusters so far. It's also the id of the current cluster
-    eps: threshold on the neighborhood distance
-    min_pts: threshold on the number of neighbors to consider in order to be "core"
-    
-    return: True/False whether p_idx could expand to a cluster
-    
-    Algorithm:
-    This function runs a BFS to find the cluster defined by p_idx
-    
-    """
-    
-    seeds = region_query(points[p_idx], eps)
-    if len(seeds) < min_pts:
-        cluster_id[p_idx] = -1
-        return False 
-    for q_idx in seeds:
-        cluster_id[q_idx] = n_clusters
+    if metric == 'euclidean':
+        return np.sqrt(np.sum((p1 - p2)**2))
+    elif metric == 'manhattan':
+        return np.sum(np.abs(p1 - p2))
+    elif metric == 'cosine':
+        return 1 - np.dot(p1, p2) / (np.linalg.norm(p1) * np.linalg.norm(p2))
+    else:
+        raise ValueError('Unknown metric')
+class DBSCAN:
+    def __init__(self, eps, min_pts, metric='euclidean') -> None:
+        self.eps = eps 
+        self.min_pts = min_pts
+        self.metric = metric
         
-    i = 0
-    while i < len(seeds):
-        p_idx = seeds[i]
-        p = points[p_idx]
-        neighborhood = region_query(points, p, eps)
-        if len(neighborhood) < min_pts:
-            continue 
-        
-        for q_idx in neighborhood:
-            if cluster_id[q_idx] is None or cluster_id[q_idx] == -1:
-                if cluster_id[q_idx] == -1:
-                    seeds.append(q_idx)
-                cluster_id[q_idx] = n_clusters
-    return True 
-                    
+    def fit(self, X):
+        """
+        X: array of points
+        """
+        n = len(X)
+        self.labels = [None for _ in range(n)] # None means unlabeled, -1 means noise
+        self.n_clusters = 0
 
-def dbscan():
-    global cluster_id
-    n_clusters = 0
-    n = len(points)
-    
-    cluster_id = [None for _ in range(n)] # None: unclassified, -1: Noise
-    
-    for i in range(n):
-        if cluster_id[i] is None and expand_cluster(points, i, n_clusters, eps, min_pts):
-            n_clusters += 1
+        for i in range(n):
+            if self.labels[i] is None and self._expand_cluster(X, i):
+                self.n_clusters += 1
+                        
+                
+    def fit_predict(self, X):
+        self.fit(X)
+        return self.labels    
+        
+    def _region_query(self, X, p):
+        """
+        p: a point
+        eps: threshold on the neighborhood distance
+
+        return: list of ids of points at distance < eps from p
+
+        (done with Brute Force now)
+        """
+        
+        n = len(X)
+        result_ids = [i for i in range(n) if distance(X[i], p, self.metric) < self.eps]
+        return result_ids
+
+    def _expand_cluster(self, X, p_idx):
+        """
+        p_idx: id of the point candidate to be core of a cluster
+        
+        return: True/False whether p_idx could expand to a cluster or not
+        
+        Algorithm:
+        This function runs a BFS to find the cluster defined by p_idx
+        """
+        
+        seeds = self._region_query(X, X[p_idx])
+        if len(seeds) < self.min_pts:
+            self.labels[p_idx] = -1 # Noise
+            return False 
+        
+        for q_idx in seeds:
+            self.labels[q_idx] = self.n_clusters
             
-        
+        i = 0
+        while i < len(seeds):
+            p_idx = seeds[i]
+            p = X[p_idx]
+            neighborhood = self._region_query(X, p)
+            if len(neighborhood) >= self.min_pts:    
+                for q_idx in neighborhood:
+                    if self.labels[q_idx] is None or self.labels[q_idx] == -1:
+                        if self.labels[q_idx] == -1:
+                            seeds.append(q_idx)
+                        self.labels[q_idx] = self.n_clusters
+            i += 1
+        return True 
+                        
+
+def test():
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    X, _ = make_blobs(
+        n_samples=750, centers=centers, cluster_std=0.4, random_state=0
+    )
+
+    X = StandardScaler().fit_transform(X)
+    
+    
+    dbscan = DBSCAN(eps=1, min_pts=5)
+    labels = dbscan.fit_predict(X)
+    
+    print()
+    print("Number of noise points:", labels.count(-1))
+    print("Number of clusters:", dbscan.n_clusters)
+    print()
+    
+    plt.scatter(X[:, 0], X[:, 1], c=labels)
+    plt.show()
+
+if __name__ == '__main__':
+    test()
