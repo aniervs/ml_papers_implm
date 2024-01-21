e8c50cbdf646a7dec7963f6b3735dd23ff077287
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2024-01-21 02:44:14 +0100

    FIX the implementation of DBSCAN. Add README. Add examples.

---
+++ cluster/README.md
@@ -0,0 +1,10 @@
+# Cluster algorithms
+## DBSCAN
+
+- Reference paper: [A Density-Based Algorithm for Discovering Clusters in Large Spatial Databases with Noise](https://www.dbs.ifi.lmu.de/Publikationen/Papers/KDD-96.final.frame.pdf)
+
+- Official Scikit-Learn documentation: [sklearn.cluster.DBSCAN](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html)
+
+### Example usage
+
+<img src="assets/dbscan_blobs_moon.png"/>
--- cluster/dbscan.py
+++ cluster/dbscan.py
@@ -4,6 +4,8 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs, make_moons 
 from sklearn.preprocessing import StandardScaler
+from collections import deque
+    
 
 def distance(p1, p2, metric='euclidean'):
     """
@@ -20,6 +22,20 @@ def distance(p1, p2, metric='euclidean'):
         return 1 - np.dot(p1, p2) / (np.linalg.norm(p1) * np.linalg.norm(p2))
     else:
         raise ValueError('Unknown metric')
+    
+def region_query(X, p, eps, metric='euclidean'):
+    """
+    X: dataset (n x d)
+    p: point (d)
+    eps: epsilon (float)
+    metric: distance metric (str)
+    
+    return: all points in X within eps of p
+    (done with Brute Force now)
+    """
+    neighbors = [i for i in range(X.shape[0]) if distance(X[i], p, metric) < eps]
+    return neighbors
+
 class DBSCAN:
     def __init__(self, eps, min_pts, metric='euclidean') -> None:
         self.eps = eps 
@@ -31,66 +47,47 @@ class DBSCAN:
         X: array of points
         """
         n = len(X)
-        self.labels = [None for _ in range(n)] # None means unlabeled, -1 means noise
+        
+        self.neighborhoods = [region_query(X, X[i], self.eps, self.metric) for i in range(n)]
+        self.labels = np.array([-1 for _ in range(n)]) # by default everything is Noise
+        self.is_core = np.array([len(self.neighborhoods[i]) >= self.min_pts for i in range(n)])
+        
         self.n_clusters = 0
-
+        
         for i in range(n):
-            if self.labels[i] is None and self._expand_cluster(X, i):
+            if self.is_core[i] and self.labels[i] == -1:
+                self._bfs(i)
                 self.n_clusters += 1
+                
+        return self
                         
                 
     def fit_predict(self, X):
         self.fit(X)
-        return self.labels    
-        
-    def _region_query(self, X, p):
-        """
-        p: a point
-        eps: threshold on the neighborhood distance
+        return self.labels
 
-        return: list of ids of points at distance < eps from p
-
-        (done with Brute Force now)
+    def _bfs(self, p_id):
         """
-        
-        n = len(X)
-        result_ids = [i for i in range(n) if distance(X[i], p, self.metric) < self.eps]
-        return result_ids
-
-    def _expand_cluster(self, X, p_idx):
-        """
-        p_idx: id of the point candidate to be core of a cluster
-        
-        return: True/False whether p_idx could expand to a cluster or not
+        p_id: id of the core point taken to expand on the current cluster
         
         Algorithm:
-        This function runs a BFS to find the cluster defined by p_idx
+        This function runs a BFS to find the cluster defined by p_id
         """
         
-        seeds = self._region_query(X, X[p_idx])
-        if len(seeds) < self.min_pts:
-            self.labels[p_idx] = -1 # Noise
-            return False 
-        
-        for q_idx in seeds:
-            self.labels[q_idx] = self.n_clusters
-            
-        i = 0
-        while i < len(seeds):
-            p_idx = seeds[i]
-            p = X[p_idx]
-            neighborhood = self._region_query(X, p)
-            if len(neighborhood) >= self.min_pts:    
-                for q_idx in neighborhood:
-                    if self.labels[q_idx] is None or self.labels[q_idx] == -1:
-                        if self.labels[q_idx] == -1:
-                            seeds.append(q_idx)
-                        self.labels[q_idx] = self.n_clusters
-            i += 1
-        return True 
-                        
+        q = deque()
+        q.append(p_id)
+        self.labels[p_id] = self.n_clusters
 
-def test():
+        while len(q) > 0:
+            p_id = q.popleft() 
+            neighbors = self.neighborhoods[p_id]
+            for q_id in neighbors:
+                if self.labels[q_id] == -1:
+                    self.labels[q_id] = self.n_clusters
+                    if self.is_core[q_id]:
+                        q.append(q_id)
+                        
+def run_blobs(ax):
     centers = [[1, 1], [-1, -1], [1, -1]]
     X, _ = make_blobs(
         n_samples=750, centers=centers, cluster_std=0.4, random_state=0
@@ -98,17 +95,48 @@ def test():
 
     X = StandardScaler().fit_transform(X)
     
-    
-    dbscan = DBSCAN(eps=1, min_pts=5)
+    dbscan = DBSCAN(eps=0.3, min_pts=10)
     labels = dbscan.fit_predict(X)
     
     print()
-    print("Number of noise points:", labels.count(-1))
-    print("Number of clusters:", dbscan.n_clusters)
+    print("BLOBS Number of noise points:", np.sum(labels == -1))
+    print("BLOBS Number of clusters:", dbscan.n_clusters)
     print()
     
-    plt.scatter(X[:, 0], X[:, 1], c=labels)
+    ax.set_title('Blobs')
+    
+    for c_id in range(dbscan.n_clusters):
+        ax.scatter(X[labels == c_id, 0], X[labels == c_id, 1], label=f'Cluster {c_id}', c=f'C{c_id}')    
+    ax.scatter(X[labels == -1, 0], X[labels == -1, 1], label='Noise', c='black')
+    ax.legend()
+    
+
+def run_moons(ax):
+    X, _ = make_moons(n_samples=750, noise=0.05, random_state=0)
+    X = StandardScaler().fit_transform(X)
+    
+    dbscan = DBSCAN(eps=0.3, min_pts=10)
+    labels = dbscan.fit_predict(X)
+    
+    print()
+    print("MOONS Number of noise points:", np.sum(labels == -1))
+    print("MOONS Number of clusters:", dbscan.n_clusters)
+    print()
+    
+    ax.set_title('Moons')
+    
+    for c_id in range(dbscan.n_clusters):
+        ax.scatter(X[labels == c_id, 0], X[labels == c_id, 1], label=f'Cluster {c_id}', c=f'C{c_id}')    
+    ax.scatter(X[labels == -1, 0], X[labels == -1, 1], label='Noise', c='black')
+    ax.legend()
+
+def main():
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    
+    run_blobs(ax1)
+    run_moons(ax2)
+    
     plt.show()
 
 if __name__ == '__main__':
-    test()
+    main()
